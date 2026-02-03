1d776205a161a5edac07ae2d5aa88f6d89ddfd47
NewtalentsG <newtalents403@gmail.com>
2026-02-03 16:26:15 +0200

    Update _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,10 @@
+import { AuthProvider } from "@/lib/AuthContext";
 import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <AuthProvider>
+      <Component {...pageProps} />
+    </AuthProvider>
+  );
 }
